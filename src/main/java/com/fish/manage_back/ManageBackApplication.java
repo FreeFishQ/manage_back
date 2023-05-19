02f847be83b4b53778586e476f82230bcb2101cf
FreeFishQ <hejd0811@163.com>
2023-05-19 10:10:37 +0800

    init

---
+++ src/main/java/com/fish/manage_back/ManageBackApplication.java
@@ -0,0 +1,13 @@
+package com.fish.manage_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fish/manage_back/ManageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fish.manage_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
